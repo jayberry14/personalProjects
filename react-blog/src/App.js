7f5882d1519664bdb45261e3321e5604fbc340d7
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-06-11 09:12:12 -0600

    Adding React Project

---
+++ react-blog/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/HomePage';
+import {
+  BrowserRouter as Router,
+  Route 
+} from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
+import ArticlesList from './pages/ArticlesList';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './navbar';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar /> 
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact/>
+          <Route path="/about" component={AboutPage}/>
+          <Route path="/articles-list" component={ArticlesList}/>
+          <Route path="/article/:name" component={ArticlePage}/>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
