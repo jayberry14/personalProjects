614fd3e283407c892381ab5a16453c65f0c717ff
Josh Mayberry <mayberryjosh.jm@gmail.com>
2020-08-04 15:30:43 -0600

    Updated WebProfile

--- WebProfile/RideShare/rideshare.php
+++ WebProfile/RideShare/rideshare.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <link rel='stylesheet' type='text/css' media='screen' href='directory.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='../directory.css'>
     <!-- <script src='directory.js'></script> -->
     <script>
 			// prevents refresh from submitting form and clears out unneeded variables
