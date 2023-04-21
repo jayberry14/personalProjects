4a95ad316d0d33834ce42877be5bdbafbcf30275
Josh Mayberry <mayberryjosh.jm@gmail.com>
2023-04-21 18:52:18 -0500

    Refactoring folder structure

--- Documents/resumeProjects/React/react-blog/src/pages/404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const page404 = () => (
-    <h1>404: Page Not Found</h1>
-);
-
-export default page404;
--- Documents/resumeProjects/Zeus/e-commerce/src/views/manageProducts.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ItemsContent from './ItemContent';
-
-const manageProducts = ({ match }) => {
-    null;
-}
-
-export default manageProducts;
